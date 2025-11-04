2d54f6d5b82b7190bf82270edbbd4bae83bc9684
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-11-04 11:32:21 +0700

    #8 Tạo phần Navbar cho dự án

---
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,76 @@
+import { Checkbox, Rate } from "antd";
+import { WrapperContent, WrapperLableText, WrapperTextValue, WrapperTextPrice } from "./style";
+
+const NavbarComponent = () => {
+  const onChange = () => {};
+  const renderContent = (type, options) => {
+    switch (type) {
+      case "text":
+        return options.map((option) => {
+          return <WrapperTextValue>{option}</WrapperTextValue>;
+        });
+      case "checkbox":
+        return (
+          <Checkbox.Group
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "12px",
+            }}
+            onChange={onChange}
+          >
+            {options.map((option) => {
+              return (
+                <Checkbox style={{ marginLeft: 0 }} value={option.value}>
+                  {option.label}
+                </Checkbox>
+              );
+            })}
+            <Checkbox value="B">B</Checkbox>
+          </Checkbox.Group>
+        );
+      case "star":
+        return options.map((option) => {
+          console.log("check", options);
+          return (
+            <div style={{ display: "flex", gap: "4px" }}>
+              <Rate
+                style={{ fontSize: "12px" }}
+                disabled
+                defaultValue={option}
+              />
+              <span>{`từ ${option} sao`}</span>
+            </div>
+          );
+        });
+      case "price":
+        return options.map((option) => {
+          return (
+            <WrapperTextPrice>{option}</WrapperTextPrice>
+          );
+        });
+      default:
+        return {};
+    }
+  };
+  return (
+    <div>
+      <WrapperLableText>Label</WrapperLableText>
+      <WrapperContent>
+        {renderContent("text", ["Tu Lanh", "TV", "MAYGIAT"])}
+      </WrapperContent>
+      <WrapperContent>
+        {renderContent("checkbox", [
+          { value: "a", label: "A" },
+          { value: "b", label: "B" },
+        ])}
+      </WrapperContent>
+      <WrapperContent>{renderContent("star", [3, 4, 5])}</WrapperContent>
+      <WrapperContent>
+        {renderContent("price", ["duoi 40", "tren 50.000 "])}
+      </WrapperContent>
+    </div>
+  );
+};
+export default NavbarComponent;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import slider1 from "../../assets/images/slider1.jpg";
 import slider2 from "../../assets/images/slider2.jpg";
 import slider3 from "../../assets/images/slider3.jpg";
 import CardComponent from "../../components/CardComponent/CardComponent";
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
 const HomePage = () => {
   const arr = ["TV", "Tu lanh", "Laptop"];
   return (
@@ -18,12 +19,24 @@ const HomePage = () => {
       </div>
       <div
         id="container"
-        style={{ backgroundColor: "#efefef", padding: "0 120px", height: '1000px' }}
+        style={{
+          backgroundColor: "#efefef",
+          padding: "0 120px",
+          height: "1000px",
+        }}
       >
         <SliderComponent arrImages={[slider1, slider2, slider3]} />
-        <div style={{marginTop: '20px', display: 'flex', alignItems: 'center', gap: '20px'}}>
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
           <CardComponent />
         </div>
+        <NavbarComponent />
       </div>
     </>
   );
